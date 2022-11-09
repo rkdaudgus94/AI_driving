f4b9c4b7914b0476c4d379e30299614e3ce28f8c
tae2On <djs99870@naver.com>
2022-11-09 23:24:55 +0900

    Create 3.py

---
+++ eon/3.py
@@ -0,0 +1,77 @@
+import RPi.GPIO as IO
+import time
+
+IO.setmode(IO.BCM)
+
+pwmPin = 4
+AIN1 = 17
+AIN2 = 27
+encPinA = 22
+encPinB = 18
+
+IO.setwarnings(False)
+IO.setup(encPinA, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(encPinB, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(pwmPin,IO.OUT, initial=IO.LOW)
+IO.setup(AIN1,IO.OUT, initial=IO.LOW)
+IO.setup(AIN2,IO.OUT, initial=IO.LOW)
+
+p = IO.PWM(4, 100)
+p.start(0)
+
+encoderPos = 0
+
+def encoderA(channel):
+    global encoderPos
+    if IO.input(encPinA) == IO.input(encPinB):
+        encoderPos += 1
+    else:
+        encoderPos -= 1
+   
+def encoderB(channel):
+    global encoderPos
+    if IO.input(encPinA) == IO.input(encPinB):
+        encoderPos -= 1
+    else:
+        encoderPos += 1
+
+IO.add_event_detect(encPinA, IO.BOTH, callback=encoderA)
+IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
+
+targetDeg= 360.
+ratio = 360./30./168. #168 OR 206
+Kp = 1000.
+Kd = 0.
+Ki = 0.
+dt = 0.
+dt_sleep = 0.01
+tolerance = 0.01
+
+start_time = time.time()
+error_prev = 0.
+time_prev = 0.
+
+while True:
+    motorDeg = encoderPos * ratio
+
+    error = targetDeg - motorDeg
+    de = error-error_prev
+    dt = time.time() - time_prev
+    control = Kp*error + Kd*de/dt + Ki*error*dt
+
+    error_prev = error
+    time_prev = time.time()
+   
+    IO.output(AIN1, control >= 0)
+    p.ChangeDutyCycle(min(abs(control), 100))
+    
+    print('P-term = %7.1f, D-term = %7.1f, I-term = %7.1f' %(Kp*error, Kd*de/dt, Ki*de*dt))
+    print('time = %6.3f, enc = %d, deg = %5.1f, err = %5.1f, ctrl = %7.1f' %(time.time()-start_time, encoderPos, motorDeg, error, control))
+    print('%f, %f' %(de, dt))
+ 
+    if abs(error) <= tolerance:
+        IO.output(AIN1, control >= 0)
+        p.ChangeDutyCycle(0)
+        break
+   
+    time.sleep(dt_sleep)
