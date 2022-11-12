5b31776c134152d5a2167a0dd3448978efb62f06
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-11-12 16:17:37 +0900

    Update 4.py

--- eon/4.py
+++ eon/4.py
@@ -41,7 +41,7 @@ IO.add_event_detect(encPinA, IO.BOTH, callback=encoderA)
 IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
 
 # PID 제어
-ratio = 360./90./41. # 한 바퀴에 약 4100펄스
+ratio = 360./90./52. # 한 바퀴에 약 4100펄스
 
 setha = 0
 
