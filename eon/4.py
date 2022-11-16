81c0b40085a1812957c3ac2c08e1863d4eb940da
ssun0402 <109329261+ssun0402@users.noreply.github.com>
2022-11-16 23:41:25 +0900

    Update 4.py

--- eon/4.py
+++ eon/4.py
@@ -45,7 +45,7 @@ IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
 ratio = 360./90./52. # 한 바퀴에 약 4100펄스
 
 # PID 상수
-kp = 10000.
+kp = 10.
 kd = 0.
 ki = 0.
 
@@ -75,8 +75,8 @@ try:
 
         # 역방향
         if(setha < 0) :
-            IO.output(AIN1, IO.HIGH)
-            IO.output(AIN2, IO.LOW)
+            IO.output(AIN1, IO.LOW)
+            IO.output(AIN2, IO.HIGH)
 
             # seta가 모터 각도보다 크고 control이 플러스 값이 나오게 되면 모터가 멈춤
             if ((setha >= motorDeg) & (control >= 0)) :
@@ -89,8 +89,8 @@ try:
 
         # 정방향
         elif (setha > 0) :
-            IO.output(AIN1, IO.LOW)
-            IO.output(AIN2, IO.HIGH)
+            IO.output(AIN1, IO.HIGH)
+            IO.output(AIN2, IO.LOW)
 
             # seta가 모터 각도보다 작고 control이 마이너스 값이 나오게 되면 모터가 멈춤
             if((setha <= motorDeg) & (control <= 0)) :
