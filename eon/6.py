53f569e3f15ce7ebf28716da335a348c1898af83
tae2On <djs99870@naver.com>
2022-11-21 23:32:59 +0900

    Update 6.py

--- eon/6.py
+++ eon/6.py
@@ -23,8 +23,9 @@ GPIO.setup(PWMA, GPIO.OUT, initial=GPIO.LOW)
 
 #PWM 객체 인스턴스 작성(출력 핀:12, 주파수 100)
 p = GPIO.PWM(PWMA, 100)
-p.start(100)
+p.start(0)
 
-GPIO.output(AIN1, GPIO.HIGH)
-GPIO.output(AIN2, GPIO.LOW)
-p.ChangeDutyCycle(100) 
+while(1) :  
+    GPIO.output(AIN1, GPIO.HIGH)
+    GPIO.output(AIN2, GPIO.LOW)
+    p.ChangeDutyCycle(100) 
