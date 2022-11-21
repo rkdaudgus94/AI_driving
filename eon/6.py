9334f05723f97ada2aaeb86179f01057d1c2c2a6
tae2On <djs99870@naver.com>
2022-11-21 18:12:27 +0900

    Update 6.py

--- eon/6.py
+++ eon/6.py
@@ -2,8 +2,8 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO_TRIGGER = 23
-GPIO_ECHO = 24
+GPIO_TRIGGER = 2
+GPIO_ECHO = 3
 print("start")
 
 GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
