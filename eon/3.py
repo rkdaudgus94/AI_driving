b722b565c4667244102edf2020017a8dea9df5f1
tae2On <djs99870@naver.com>
2022-11-23 20:39:17 +0900

    Update 3.py

--- eon/3.py
+++ eon/3.py
@@ -73,7 +73,7 @@ try:
         IO.output(AIN1, control >= 0)
         p.ChangeDutyCycle(min(abs(control), 100))
 
-        IO.output(AIN2, control <= 0)
+        IO.output(AIN2, IO.LOW)
         p.ChangeDutyCycle(min(abs(control), 0))
 
         print('P-term = %7.1f, D-term = %7.1f, I-term = %7.1f' %(kp*error, kd*de/dt, ki*de*dt))
