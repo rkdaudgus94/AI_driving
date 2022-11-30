b1b82bd4624944b0d93142ef0a40c3960bc81982
tae2On <djs99870@naver.com>
2022-11-30 17:49:08 +0900

    Update 7.py

--- eon/7.py
+++ eon/7.py
@@ -1,33 +1,103 @@
-# DC모터 속도/방향 조절하기 
-# 천천히 회전하기 시작해서 회전 속도를 서서히 올리다가 최고 속도가 되면 다시 천천히 회전 속도를 낮춰서 정지 (회전 방향 바꿈)
+# 360도 회전하기 
 
-
-import RPi.GPIO as GPIO
+import RPi.GPIO as IO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+pwmPin = 14 # 모터드라이버 ENA
+AIN1 = 15 # IN 1
+AIN2 = 18 # IN 2
+encPinA = 2 # 보라색 (A)
+encPinB = 3 # 파랑색 (B)
 
-AIN1= 15
-AIN2= 18 
-PWMA= 14
+IO.setmode(IO.BCM)
+IO.setwarnings(False)
+IO.setup(encPinA, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(encPinB, IO.IN, pull_up_down=IO.PUD_UP)
+IO.setup(pwmPin, IO.OUT, initial=IO.LOW)
+IO.setup(AIN1, IO.OUT, initial=IO.LOW)
+IO.setup(AIN2, IO.OUT, initial=IO.LOW)
 
-GPIO.setmode(GPIO.BCM)
-
-# 듀티 비를 변화시킬 스텝 정의 
-c_step = 10   
-
-#각 핀을 출력 핀으로 설정 
-GPIO.setup(AIN1, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(AIN2, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(PWMA, GPIO.OUT, initial=GPIO.LOW)
-
-#PWM 객체 인스턴스 작성(출력 핀:12, 주파수 100)
-p = GPIO.PWM(PWMA, 100)
+p = IO.PWM(14, 100)
 p.start(0)
 
-try:
-	while 1:
-		GPIO.output(AIN1, GPIO.HIGH)
-        GPIO.output(AIN2, GPIO.LOW)
-		p.ChangeDutyCycle(100)
+encoderPos = 0
 
+def encoderA(encPinA):
+    global encoderPos
+    if IO.input(encPinA) == IO.input(encPinB):
+        encoderPos += 1 
+    else:
+        encoderPos -= 1
+   
+def encoderB(encPinB):
+    global encoderPos
+    if IO.input(encPinA) == IO.input(encPinB):
+        encoderPos -= 1
+    else:
+        encoderPos += 1
+
+IO.add_event_detect(encPinA, IO.BOTH, callback=encoderA)
+IO.add_event_detect(encPinB, IO.BOTH, callback=encoderB)
+
+# 원하는 각도
+targetDeg = 360.
+
+# PID 제어
+ratio = 360./90./52.
+
+# PID 상수
+kp = float(input("KP:"))   #0.5
+kd = float(input("KD:"))   #0.4
+ki = float(input("KI:"))   #0.3
+
+dt = 0.
+di = 0.
+dt_sleep = 0.01
+tolerance = 0.1
+
+start_time = time.time()
+error_prev = 0.
+#error_prev_prev = 0.
+time_prev = 0.
+#control = 0.
+try:
+    while True:
+        motorDeg = encoderPos * ratio
+
+        error = targetDeg - motorDeg
+        de = error - error_prev
+        di += error * dt 
+        dt = time.time() - time_prev
+        control = (kp*error) + (kd*de/dt) + (ki*di)
+
+        error_prev = error
+        
+        #delta_v = kp*de + ki*error + kd*(error - 2*error_prev + error_prev_prev)
+        #control += delta_v
+        #error_prev = error
+        #error_prev_prev = error_prev
+        
+        IO.output(AIN1, control >= 0)
+        IO.output(AIN2, control <= 0)
+        time.sleep(0.5)
+        p.ChangeDutyCycle(min(abs(control), 100))
+
+        print('P-term = %7.1f, D-term = %7.1f, I-term = %7.1f' %(kp*error, kd*de/dt, ki*de*dt))
+        print('time = %6.3f, enc = %d, deg = %5.1f, err = %5.1f, ctrl = %7.1f' %(time.time()-start_time, encoderPos, motorDeg, error, control))
+        print('%f, %f' %(de, dt))
+    
+        if abs(error) <= tolerance :
+            IO.output(AIN1, control >=0)
+            IO.output(AIN2, control <=0)
+            time.sleep(0.5)
+            p.ChangeDutyCycle(0)
+            break
+
+        time_prev = time.time()
+        time.sleep(dt_sleep)
+
+    # Crtl + c 누르면 모터 작동 멈춤
+except KeyboardInterrupt: 
+    pass
+
+p.stop() 
